03a6de8b5fede0cdc12a5118d6c6936681f0374d
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2022-12-26 15:05:06 +0700

    add configuration

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
-
+app.get('/', (req, res) => {
+    res.send('Welcome to API Kuliner.Go')
+})
 app.listen(5000, ()=> console.log('Server up and running...'));
+
+module.exports = app
