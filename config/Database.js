7cf563a989a6f080f66d2f9380e9fa57d7ffdee9
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-06-24 15:56:04 +0700

    add .env, update database.js

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('kuliner_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql"
 });
+
 
 console.log("Database Connected!")
 export default db;
