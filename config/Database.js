e20eac08707a0a72881f166b5ccc61f93fd8a8a0
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-06-24 16:35:08 +0700

    update .env and database.js

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = new Sequelize(process.env.DB_USERNAME, process.env.DB_DBNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql"
 });
 
-
-console.log("Database Connected!")
+console.log("Database Connected!");
 export default db;
