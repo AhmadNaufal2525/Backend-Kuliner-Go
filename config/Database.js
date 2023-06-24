ae1b30f89157da95f89903e1fcff2f82de85c917
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-06-24 16:12:52 +0700

    update env and database.js

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_USERNAME, process.env.DB_DBNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql"
 });
